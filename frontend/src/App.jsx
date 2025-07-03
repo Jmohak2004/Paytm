7a3906789b30d40fd3bc88ec75c947f5625e3d6d
Mohak Jaiswal <mohakjaiswal@Mac.lan>
2025-07-03 20:11:54 +0530

    Routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/SignUp";
+import { Signin } from "./pages/SignIn";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
 
   return (
     <div>
-        Hello world
+      <Routes>
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney/>} />
+      </Routes>
+
     </div>
   )
 }
